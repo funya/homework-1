440eb7f17203d5fcdc1fbd8155e9cec964c075ab
Timur Myakinin <myakinin@gmail.com>
2019-07-29 22:02:44 +0300

    [+] add App component (div.App > p.description)

---
+++ src/components/App/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class App extends React.Component {
+    render() {
+        return <div className = "App">
+            <p className = "description">Миру-мир, студентам - beer.</p>
+        </div>;
+    }
+};
+
+export default App;
